525e32cc8131f963dbcf7d69b0376ff903afc953
Sujin Gwak <kasiuky@nate.com>
2022-12-13 02:47:47 +0900

    linc project deploy

--- src/main/java/CatholicUniversity/Linc/repository/BucheonFilesRepository.java
+++
@@ -1,9 +0,0 @@
-package CatholicUniversity.Linc.repository;
-
-import CatholicUniversity.Linc.domain.entity.BucheonFiles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BucheonFilesRepository extends JpaRepository<BucheonFiles, Long> {
-}
